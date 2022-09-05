6d19690f014ce7733e0e886d69edc5384d0fe5bd
Alimoff <sobiriddin@gmail.com>
2022-09-05 19:46:24 +0500

    championat model added

--- football/models.py
+++ football/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Chempionat(models.Model):
+    match = models.CharField("Championat:", max_length=200)
+    country = models.CharField("Country:", max_length=200)
+    date = models.CharField("Date:", max_length=200)
+    time = models.CharField("Time:", max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+
+        verbose_name = "Match"
+        verbose_name_plural = "Matches"
